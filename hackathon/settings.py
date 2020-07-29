aff9b8746c5f4c659308d12fb89c318f22558c17
Cam <cvl8ec@virginia.edu>
2020-07-29 10:00:05 -0700

    serve logo as static asset in development

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'hackathon.wsgi.application'
 #https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #comment for local development
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('DATABASE_NAME'),
+#         'USER': os.getenv('DATABASE_USER'),
+#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+#         'HOST': os.getenv('DATABASE_HOST'),
+#         'PORT': os.getenv('DATABASE_PORT')
+#     }
+# }
 
 #uncomment for local development
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+} 
 
 
 # Password validation
