bb09030230f220a43991595d5e357e6af72c91f3
Cam <cvl8ec@virginia.edu>
2020-07-29 07:44:38 -0700

    add deadline field to matchup model, default is 1 week after now

--- bracket/models.py
+++ bracket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta
 
 class Team(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -14,6 +15,7 @@ class Team(models.Model):
 class Matchup(models.Model):
     team1 = models.ForeignKey("Team", related_name = 'team1', on_delete=models.CASCADE)
     team2 = models.ForeignKey("Team", related_name = 'team2', on_delete=models.CASCADE)
+    deadline = models.DateTimeField(default=datetime.now() + timedelta(days=7))
     def getWinner(self):
         return team1 if team1.vote_count > team2.vote_count else team2
     def clean(self):
