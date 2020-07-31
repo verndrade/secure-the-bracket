a3cd0794632af92bc3e3a8eed0c0fda764ae0411
Vernon Andrade <t-veand@microsoft.com>
2020-07-30 21:34:44 -0400

    change landing page

--- bracket/urls.py
+++ bracket/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'bracket'
 urlpatterns = [
     path('', views.home, name="home"),
-    path('panel', views.panel, name="panel"),
     path('team/<slug:name>', views.team, name="teams"),
     path('matchup/<slug:id>', views.matchup, name="matchup"),
     path('campaign/<slug:id>', views.campaign, name="campaign"),
